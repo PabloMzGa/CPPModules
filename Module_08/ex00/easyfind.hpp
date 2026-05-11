eea90dcc5034196c40e4045b8c3aa69530509d0e
Pablo Martinez Garcia <pabmart2@c2r4s5.42malaga.com>
2026-05-11 20:43:44 +0200

    M08/ex00

--- Module_08/ex00/easyfind.hpp
+++ Module_08/ex00/easyfind.hpp
@@ -6,29 +6,45 @@
 /*   By: pabmart2 <pabmart2@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/05 20:25:09 by pabmart2          #+#    #+#             */
-/*   Updated: 2026/05/07 19:45:24 by pabmart2         ###   ########.fr       */
+/*   Updated: 2026/05/11 20:03:38 by pabmart2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
-#define EASYFIND_HPP
+# define EASYFIND_HPP
 
-#include <algorithm>
+# include <algorithm>
 
-template <typename A, typename B>
-struct is_same { static const bool value = false; };
-
-template <typename A>
-struct is_same<A, A> { static const bool value = true; }; // ✔️ LEGAL
-
-/*
-template <typename Container>
-//"typename Container::value_type* = 0" forces the compiler to only use the template
-// in case it has a type defined inside called "value_type", typical for containers
-bool easyfind(Container &container, int value, typename Container::value_type* = 0)
+namespace easyfind_detail
 {
-	std::find(t.)
+template <typename A, typename B> struct is_same
+{
+	static const bool value = false;
+};
+
+template <typename A> struct is_same<A, A>
+{
+	static const bool value = true;
+};
+
 }
-*/
+
+/**
+ * typename Container::value_type* = 0" forces the compiler to only use the
+ * template in case it has a type defined inside called "value_type",
+ * typical for containers
+ */
+template <typename Container> typename Container::iterator easyfind(Container &container,
+	int value, typename Container::value_type * = 0)
+{
+	char	must_be_int[easyfind_detail::is_same<typename Container::value_type,
+			int>::value ? 1 : -1];
+
+	// If container isn't a <int> container, the compiler will select the
+	// template which returns false. That way, must_be_int will have size -1
+	// and an compilation error will ouccur
+	(void)must_be_int;
+	return (std::find(container.begin(), container.end(), value));
+}
 
 #endif
--- Module_08/ex00/main.cpp
+++ Module_08/ex00/main.cpp
@@ -6,15 +6,76 @@
 /*   By: pabmart2 <pabmart2@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/07 19:35:50 by pabmart2          #+#    #+#             */
-/*   Updated: 2026/05/07 19:46:12 by pabmart2         ###   ########.fr       */
+/*   Updated: 2026/05/11 20:03:23 by pabmart2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <deque>
 #include <iostream>
+#include <list>
+#include <vector>
+
 #include "easyfind.hpp"
 
-int main ()
+template <typename Container>
+static void print_result(Container &container, int value)
 {
-	std::cout << "Ret: " << is_same<int, bool>::value << std::endl;
-		std::cout << "Ret: " << is_same<int, int>::value << std::endl;
+	typename Container::iterator it = easyfind(container, value);
+
+	std::cout << "Searching " << value << ": ";
+	if (it == container.end())
+		std::cout << "not found" << std::endl;
+	else
+		std::cout << "found at value " << *it << std::endl;
+}
+
+int main()
+{
+	std::vector<int> numbers;
+	for (int i = 10; i <= 50; i += 10)
+		numbers.push_back(i);
+
+	std::list<int> values;
+	values.push_back(1);
+	values.push_back(3);
+	values.push_back(5);
+	values.push_back(7);
+
+	std::deque<int> more_values;
+	more_values.push_back(2);
+	more_values.push_back(4);
+	more_values.push_back(6);
+
+	print_result(numbers, 30);
+	print_result(numbers, 99);
+	print_result(values, 5);
+	print_result(values, 8);
+	print_result(more_values, 4);
+	print_result(more_values, 9);
+
+	/*
+	** Compile-time failure examples:
+	** 1) A container with value_type, but not int, is rejected by is_same.
+	** 2) A type without value_type is rejected by the SFINAE parameter.
+	** easyfind() only accepts containers that expose value_type and are
+	** containers of int.
+	** If you uncomment the block below, the type has no value_type, so the
+	** template argument substitution fails before easyfind() can be used.
+	*/
+	// In both cases, compilation would stop if these calls were enabled.
+	// The first one fails because is_same<value_type, int>::value is false.
+	// The second one fails earlier, because the default parameter tries to
+	// use Container::value_type and the substitution is discarded.
+#if 0
+	std::vector<std::string> words;
+	words.push_back("42");
+	words.push_back("CPP");
+	easyfind(words, 42);
+
+	struct NoValueType {};
+	NoValueType	no_value_type;
+	easyfind(no_value_type, 42);
+
+#endif
+	return (0);
 }
